60a514e6f640fe6cdd1312cb5a734df055f2859c
chuckwu2000 <chuckwu92@gmail.com>
2023-12-25 16:50:15 +0800

    [case 1~4 correct]
    * Follow paper's method
    
    * When encounter remove step, create another vector to store delete_position, then merge old vector (but avoid delete_position) into new vector
    - Let old vector equal new vector
    
    * TREF: need to handle reference double pointer case
    - RHS: when encounter pointer that don't have name, concate a '*' to the TREF's last element, then store into TREF
    - LHS: when encounter pointer that don't have name, repeat same step equal to RHS
    - Check TREF & TEQUIV until the store instruction come, add co-alias(but no repeat) into TREF
    
    * TGEN:
    - If Operand(1) has name -> gen_name equal its name
    - If Operand(1) has noname -> LHS need reference, gen_name: cancat a '*' to the last element of TREF
    - Check TGEN & TEQUIV
    
    * DEP:
    - Flow dependence: intersection TDEF & TREF(Si)
    - Output dependence: intersection TDEF & TGEN(Si)
    
    * TDEF:
    - Remove (TGEN(Si) & TDEF)'s intersection from TDEF
    - Add TGEN to TDEF
    - Check TDEF & TEQUIV
    
    * TEQUIV: Only change when store instruction's Operand(0) is a pointer
    - Remove ((pointer point to TGEN) & TEQUIV)'s intersection
    - Set ('*'+TGEN & Operand(0)) a new equiv and add into TEQUIV
    - Check TEQUIV & TEQUIV

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -4,6 +4,7 @@
 #include "llvm/ADT/SmallVector.h"
 #include <iostream>
 #include <cstring>
+#include <algorithm>
 
 using namespace llvm;
 using namespace std;
@@ -12,6 +13,8 @@ int first_statement = 1;
 int statement_id = 1;
 SmallVector<string, 20> TREF;
 SmallVector<string, 20> TGEN;
+SmallVector<string, 20> DEP;
+SmallVector<pair<string, string>, 20> TDEF;
 SmallVector<pair<string, string>, 20> TEQUIV;
 
 string gen_name;
@@ -29,8 +32,69 @@ PreservedAnalyses HW2Pass::run(Function &F, FunctionAnalysisManager &FAM) {
 		//In this work, only one basicBlock
 		for(Instruction &I : BB)
 		{
-			if(auto *SI = dyn_cast<StoreInst>(&I))	//get TGEN info
+			//get TREF info
+			if(auto *LI = dyn_cast<LoadInst>(&I))
 			{
+				Value* val = LI->getPointerOperand();
+				if(val->hasName())
+				{		
+					ref_name = val->getName().str();
+					TREF.push_back(ref_name);
+
+				}
+				else
+				{
+					ref_name = "*" + TREF.back();
+					TREF.push_back(ref_name);
+				}
+			}
+
+			//first check TREF & TEQUIV co-alias, then get TGEN info
+			if(auto *SI = dyn_cast<StoreInst>(&I))
+			{
+				//check TEQUIV whether have the pointer point to same address
+				for(int i = 0; i < TREF.size(); i++)
+				{
+					for(int j = 0; j < TEQUIV.size(); j++)
+					{
+						if(TREF[i] == TEQUIV[j].first)
+						{
+							string TREF_add = TEQUIV[j].second;
+							int flag = 0;
+							for(int k = 0; k < TREF.size(); k++)
+							{
+								if(TREF_add == TREF[k])
+								{
+									flag = 1;
+									break;
+								}	
+							}
+							if(!flag)
+							{
+								TREF.push_back(TREF_add);
+							}
+						}
+						else if(TREF[i] == TEQUIV[j].second)
+						{
+							string TREF_add = TEQUIV[j].first;
+							int flag = 0;
+							for(int k = 0; k < TREF.size(); k++)
+							{
+								if(TREF_add == TREF[k])
+								{
+									flag = 1;
+									break;
+								}	
+							}
+							if(!flag)
+							{
+								TREF.push_back(TREF_add);
+							}
+						}
+					}
+				}
+
+				//
 				Value* val = SI->getOperand(1);
 				if(val->hasName())
 				{
@@ -56,47 +120,163 @@ PreservedAnalyses HW2Pass::run(Function &F, FunctionAnalysisManager &FAM) {
 				}
 			}
 
-			if(auto *SI = dyn_cast<StoreInst>(&I))	//get TEQUIV info
+			//get DEP info
+			if(auto *SI = dyn_cast<StoreInst>(&I))
 			{
-				Value* val = SI->getOperand(0);
-				if(val->getType()->getTypeID() == llvm::Type::PointerTyID)	//RHS is a pointer
+				//find flow dependence : intersection TREF(Si) & TDEF
+				for(int i = 0; i < TDEF.size(); i++)
 				{
-					string RHS_pointer_name = val->getName().str();
-					string LHS_pointer_name = "*" + TGEN.back();
-					TEQUIV.push_back(pair{LHS_pointer_name, RHS_pointer_name});
-					
-					//check other equiv
-					for(int i = 0; i < TEQUIV.size() - 1; i++)
+					for(int j = 0; j < TREF.size(); j++)
 					{
-						if(RHS_pointer_name == TEQUIV[i].first.substr(1))
+						if(TREF[j] == TDEF[i].first)
 						{
-							TEQUIV.push_back(pair{"*" + LHS_pointer_name, TEQUIV[i].second});
+							string flow_dep = TREF[j] + ": " + TDEF[i].second + "--->S" + to_string(statement_id);
+							DEP.push_back(flow_dep);
 						}
-						else if(RHS_pointer_name == TEQUIV[i].second.substr(1))
+					}
+				}
+
+				//find output dependence : intersection TDEF & TGEN(Si)
+				for(int i = 0; i < TDEF.size(); i++)
+				{
+					for(int j = 0; j < TGEN.size(); j++)
+					{
+						if(TGEN[j] == TDEF[i].first)
 						{
-							TEQUIV.push_back(pair{"*" + LHS_pointer_name, TEQUIV[i].first});
+							string out_dep = TGEN[j] + ": " + TDEF[i].second + "-O->S" + to_string(statement_id);
+							DEP.push_back(out_dep);
 						}
 					}
 				}
 			}
 
-
-			if(auto *LI = dyn_cast<LoadInst>(&I))	//get TREF info
+			//Update TDEF info
+			if(auto *SI = dyn_cast<StoreInst>(&I))
 			{
-				Value* val = LI->getPointerOperand();
-				ref_name = val->getName().str();
-				TREF.push_back(ref_name);
+				SmallVector<int, 20> delete_pos;
+				SmallVector<pair<string, string>, 20> next_TDEF;
+				for(int i = 0; i < TGEN.size(); i++)
+				{
+					for(int j = 0; j < TDEF.size(); j++)
+					{
+						//pointer point to the TGEN's element need to be remove
+						if(TGEN[i] == TDEF[j].first)
+						{
+							delete_pos.push_back(j);
+						}
+					}
+				}
+				std::sort(delete_pos.begin(), delete_pos.end());
+				for(int i = 0, j = 0; i < TDEF.size(); i++)
+				{
+					if(j < delete_pos.size())
+					{
+						if(i != delete_pos[j])
+						{
+							next_TDEF.push_back(TDEF[i]);
+						}
+						else
+						{
+							j++;
+						}
+					}
+					else
+					{
+						next_TDEF.push_back(TDEF[i]);
+					}
+				}
+				TDEF = next_TDEF;
+
+				for(int i = 0; i < TGEN.size(); i++)
+				{
+					TDEF.push_back(pair{TGEN[i], "S" + to_string(statement_id)});
+				}
 
 				//check TEQUIV whether have the pointer point to same address
 				for(int i = 0; i < TEQUIV.size(); i++)
 				{
-					if(ref_name == TEQUIV[i].first)
+					if(gen_name == TEQUIV[i].first)
 					{
-						TREF.push_back("[" + TEQUIV[i].second + "]");
+						for(int j = 0; j < TDEF.size(); j++)	//update TDEF
+						{
+							if(TEQUIV[i].second == TDEF[j].first)
+							{
+								TDEF[j].second = "S" + to_string(statement_id);
+							}
+						}
 					}
-					else if(ref_name == TEQUIV[i].second)
+					else if(gen_name == TEQUIV[i].second)
 					{
-						TREF.push_back("[" + TEQUIV[i].first + "]");
+						for(int j = 0; j < TDEF.size(); j++)	//update TDEF
+						{
+							if(TEQUIV[i].first == TDEF[j].first)
+							{
+								TDEF[j].second = "S" + to_string(statement_id);
+							}
+						}
+					}
+				}
+			}
+
+			//Update TEQUIV info
+			if(auto *SI = dyn_cast<StoreInst>(&I))
+			{
+				Value* val = SI->getOperand(0);
+				if(val->getType()->getTypeID() == llvm::Type::PointerTyID)	//RHS is a pointer
+				{
+					SmallVector<int, 20> delete_pos;
+					SmallVector<pair<string, string>, 20> next_TEQUIV;
+					for(int i = 0; i < TGEN.size(); i++)
+					{
+						for(int j = 0; j < TEQUIV.size(); j++)
+						{
+							//pointer point to the TGEN's element need to be remove
+							if(("*" + TGEN[i]) == TEQUIV[j].first)
+							{
+								delete_pos.push_back(j);
+							}
+						}
+					}
+					std::sort(delete_pos.begin(), delete_pos.end());
+					for(int i = 0, j = 0; i < TEQUIV.size(); i++)
+					{
+						if(j < delete_pos.size())
+						{
+							if(i != delete_pos[j])
+							{
+								next_TEQUIV.push_back(TEQUIV[i]);
+							}
+							else
+							{
+								j++;
+							}
+						}
+						else
+						{
+							next_TEQUIV.push_back(TEQUIV[i]);
+						}
+					}
+					TEQUIV = next_TEQUIV;
+
+					for(int i = 0; i < TGEN.size(); i++)
+					{
+						string LHS_pointer_name = "*" + TGEN[i];
+						string RHS_pointer_name = val->getName().str();
+						TEQUIV.push_back(pair{LHS_pointer_name, RHS_pointer_name});
+					}
+
+					int TEQUIV_size = TEQUIV.size();
+					for(int i = 0; i < TEQUIV_size - 1; i++)
+					{
+						for(int j = i + 1; j < TEQUIV_size; j++)
+						{
+							string left = TEQUIV[i].first.substr(1);
+							string right = TEQUIV[i].second;
+							if(left == TEQUIV[j].second)
+							{
+								TEQUIV.push_back(pair{"*" + TEQUIV[j].first, right});
+							}
+						}
 					}
 				}
 			}
@@ -145,13 +325,39 @@ PreservedAnalyses HW2Pass::run(Function &F, FunctionAnalysisManager &FAM) {
 				}
 				errs() << "}\n";
 
-				errs() << "DEP: {";
+				errs() << "DEP:{";
+				int first_DEP = 1;
+				for(int i = 0; i < DEP.size(); i++)
+				{
+					if(!first_DEP)
+					{
+						errs() << "    " << DEP[i] << "\n";
+					}
+					else
+					{
+						errs() << "\n    " << DEP[i] << "\n";
+						first_DEP = 0;
+					}
+				}
 				errs() << "}\n";
 
-				errs() << "TDEF: {";
+				errs() << "TDEF:{";
+				int first_TDEF = 1;
+				for(int i = 0; i < TDEF.size(); i++)
+				{
+					if(!first_TDEF)
+					{
+						errs() << ", (" << TDEF[i].first << ", " << TDEF[i].second << ")";
+					}
+					else
+					{
+						errs() << "(" << TDEF[i].first << ", " << TDEF[i].second << ")";
+						first_TDEF = 0;
+					}
+				}
 				errs() << "}\n";
 
-				errs() << "TEQUIV: {";
+				errs() << "TEQUIV:{";
 				int first_TEQUIV = 1;
 				for(int i = 0; i < TEQUIV.size(); i++)
 				{
@@ -169,6 +375,7 @@ PreservedAnalyses HW2Pass::run(Function &F, FunctionAnalysisManager &FAM) {
 
 				TREF.clear();
 				TGEN.clear();
+				DEP.clear();
 			}
 		}
 	}	
